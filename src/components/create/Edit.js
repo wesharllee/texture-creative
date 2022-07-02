4390b0b5e8fd74c25f13aa5e1f0247329c7d8389
Wesley Harllee <wesharllee@gmail.com>
2022-07-01 23:56:02 -0500

    refactored the edit page to display images

--- src/components/create/Edit.js
+++ src/components/create/Edit.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import Select from 'react-select'
+import "./Create.css"
 
 
 
@@ -45,6 +46,11 @@ export const EditPage = () => {
     const [propPackages, setPropPackages] = useState([])
     const [furniturePackages, setFurniturePackages] = useState([])
     const [lightPackages, setLightPackages] = useState([])
+
+    const [backdropsArray, setBackdropsArray] = useState([])
+    const [propsArray, setPropsArray] = useState([])
+    const [furnituresArray, setFurnituresArray] = useState([])
+    const [lightsArray, setLightsArray] = useState([])
 
     const handleBackdropChange = evt => {
         setSelectedBackdropOption(evt)
@@ -200,7 +206,7 @@ export const EditPage = () => {
             })
         }
 
-        
+
 
         fetch(`http://localhost:8080/bookingDates/${id}`, {
             method: "PUT",
@@ -343,6 +349,66 @@ export const EditPage = () => {
         []
     )
 
+    const backdropFind = () => {
+        let array = []
+        for (const selectedBackdropOption of selectedBackdropOptions) {
+            const newBackdropObj = backdrops.find(backdrop => backdrop?.id === selectedBackdropOption?.id)
+            array.push(newBackdropObj)
+        }
+        setBackdropsArray(array)
+    }
+
+    useEffect(
+        () => {
+            backdropFind()
+        }, [selectedBackdropOptions]
+    )
+
+    const propFind = () => {
+        let array = []
+        for (const selectedPropOption of selectedPropOptions) {
+            const newpropObj = props.find(prop => prop?.id === selectedPropOption?.id)
+            array.push(newpropObj)
+        }
+        setPropsArray(array)
+    }
+
+    useEffect(
+        () => {
+            propFind()
+        }, [selectedPropOptions]
+    )
+
+    const furnitureFind = () => {
+        let array = []
+        for (const selectedFurnitureOption of selectedFurnitureOptions) {
+            const newfurnitureObj = furnitures.find(furniture => furniture?.id === selectedFurnitureOption?.id)
+            array.push(newfurnitureObj)
+        }
+        setFurnituresArray(array)
+    }
+
+    useEffect(
+        () => {
+            furnitureFind()
+        }, [selectedFurnitureOptions]
+    )
+
+    const lightFind = () => {
+        let array = []
+        for (const selectedLightOption of selectedLightOptions) {
+            const newlightObj = lights.find(light => light?.id === selectedLightOption?.id)
+            array.push(newlightObj)
+        }
+        setLightsArray(array)
+    }
+
+    useEffect(
+        () => {
+            lightFind()
+        }, [selectedLightOptions]
+    )
+
     const backdropRename = () => {
         let options = []
         for (const backdrop of backdrops) {
@@ -411,129 +477,155 @@ export const EditPage = () => {
     )
 
     return (
-        <form className="createPage">
-            <h2 className="createPage__title">This will be create</h2>
+        <form className="CreatePage">
 
-            <fieldset>
-                <div>
-                    <Select
-                        isMulti
-                        placeholder="Please Select Backdrops"
-                        //capture selected option
-                        value={selectedBackdropOptions}
-                        //use the variable option that has been copied w/ added value and label
-                        options={backdropChoices}
-                        //onChange create a function to call setter function for selected option
-                        onChange={handleBackdropChange}
-                    />
-                    {/* display selected option inside of multi-select dropdown menu */}
-                    {selectedBackdropOptions && <div style={{ marginTop: 20, lineHeight: '25px' }}>
-                    </div>}
-                </div>
-            </fieldset>
+            <h2 className="createPage__title">Edit Your Experience</h2>
 
-            <fieldset>
-                <div>
-                    <Select
-                        isMulti
-                        placeholder="Please Select Props"
-                        value={selectedPropOptions}
-                        options={propChoices}
-                        onChange={handlePropChange}
-                    />
+            <section className='CreateOptions'>
 
-                    {selectedPropOptions && <div style={{ marginTop: 20, lineHeight: '25px' }}>
-                    </div>}
-                </div>
-            </fieldset>
+                <article className='CreateSelections'>
+                    <fieldset className="MultiSelect">
+                        <div className="content">
+                            <Select
+                                isMulti
+                                placeholder="Backdrop Options"
+                                //capture selected option
+                                value={selectedBackdropOptions}
+                                //use the variable option that has been copied w/ added value and label
+                                options={backdropChoices}
+                                //onChange create a function to call setter function for selected option
+                                onChange={handleBackdropChange}
+                            />
+                            {/* display selected option inside of multi-select dropdown menu */}
+                            {selectedBackdropOptions && <div style={{ marginTop: 20, lineHeight: '25px' }}>
+                            </div>}
+                        </div>
+                    </fieldset>
 
-            <fieldset>
-                <div>
-                    <Select
-                        isMulti
-                        placeholder="Please Select Furniture"
-                        value={selectedFurnitureOptions}
-                        options={furnitureChoices}
-                        onChange={handleFurnitureChange}
-                    />
+                    <fieldset className="MultiSelect">
+                        <div>
+                            <Select
+                                isMulti
+                                placeholder="Prop Options"
+                                value={selectedPropOptions}
+                                options={propChoices}
+                                onChange={handlePropChange}
+                            />
 
-                    {selectedFurnitureOptions && <div style={{ marginTop: 20, lineHeight: '25px' }}>
-                    </div>}
-                </div>
-            </fieldset>
+                            {selectedPropOptions && <div style={{ marginTop: 20, lineHeight: '25px' }}>
+                            </div>}
+                        </div>
+                    </fieldset>
 
-            <fieldset>
-                <div>
-                    <Select
-                        isMulti
-                        placeholder="Please Select Lights"
-                        value={selectedLightOptions}
-                        options={lightChoices}
-                        onChange={handleLightChange}
-                    />
+                    <fieldset className="MultiSelect">
+                        <div>
+                            <Select
+                                isMulti
+                                placeholder="Furniture Options"
+                                value={selectedFurnitureOptions}
+                                options={furnitureChoices}
+                                onChange={handleFurnitureChange}
+                            />
 
-                    {selectedLightOptions && <div style={{ marginTop: 20, lineHeight: '25px' }}>
-                    </div>}
-                </div>
-            </fieldset>
+                            {selectedFurnitureOptions && <div style={{ marginTop: 20, lineHeight: '25px' }}>
+                            </div>}
+                        </div>
+                    </fieldset>
 
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="book__date">Book Date:</label>
-                    <input
-                        required autoFocus
-                        type="date"
-                        className="form-control"
-                        value={rentalBookingPackage.date}
-                        onChange={
-                            (evt) => {
-                                const copy = { ...rentalBookingPackage }
-                                copy.date = evt.target.value
-                                updateRentalBooking(copy)
-                            }
-                        } />
-                </div>
-            </fieldset>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="book__startTime">Start Time:</label>
-                    <input
-                        required autoFocus
-                        type="time"
-                        className="form-control"
-                        value={rentalBookingPackage.startTime}
-                        onChange={
-                            (evt) => {
-                                const copy = { ...rentalBookingPackage }
-                                copy.startTime = evt.target.value
-                                updateRentalBooking(copy)
-                            }
-                        } />
-                </div>
-            </fieldset>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="book__endTime">End Time:</label>
-                    <input
-                        required autoFocus
-                        type="time"
-                        className="form-control"
-                        value={rentalBookingPackage.endTime}
-                        onChange={
-                            (evt) => {
-                                const copy = { ...rentalBookingPackage }
-                                copy.endTime = evt.target.value
-                                copy.totalHours = minus(parseFloat(copy.startTime, 2), parseFloat(copy.endTime, 2))
-                                updateRentalBooking(copy)
-                            }
-                        } />
-                </div>
-            </fieldset>
-            <button
-                onClick={(clickEvent) => editButtonClick(clickEvent, rentalPackage, rentalBookingPackage)}
-                className="package__edit">
-                Confirm Edits
-            </button>
+                    <fieldset className="MultiSelect">
+                        <div>
+                            <Select
+                                isMulti
+                                placeholder="Light Options($50)"
+                                value={selectedLightOptions}
+                                options={lightChoices}
+                                onChange={handleLightChange}
+                            />
+
+                            {selectedLightOptions && <div style={{ marginTop: 20, lineHeight: '25px' }}>
+                            </div>}
+                        </div>
+                    </fieldset>
+
+                    <fieldset>
+                        <div className="form-group">
+                            <label htmlFor="book__date">Book Date:</label>
+                            <input
+                                required autoFocus
+                                type="date"
+                                className="form-control"
+                                value={rentalBookingPackage.date}
+                                onChange={
+                                    (evt) => {
+                                        const copy = { ...rentalBookingPackage }
+                                        copy.date = evt.target.value
+                                        updateRentalBooking(copy)
+                                    }
+                                } />
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div className="form-group">
+                            <label htmlFor="book__startTime">Start Time:</label>
+                            <input
+                                required autoFocus
+                                type="time"
+                                className="form-control"
+                                value={rentalBookingPackage.startTime}
+                                onChange={
+                                    (evt) => {
+                                        const copy = { ...rentalBookingPackage }
+                                        copy.startTime = evt.target.value
+                                        updateRentalBooking(copy)
+                                    }
+                                } />
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div className="form-group">
+                            <label htmlFor="book__endTime">End Time:</label>
+                            <input
+                                required autoFocus
+                                type="time"
+                                className="form-control"
+                                value={rentalBookingPackage.endTime}
+                                onChange={
+                                    (evt) => {
+                                        const copy = { ...rentalBookingPackage }
+                                        copy.endTime = evt.target.value
+                                        copy.totalHours = minus(parseFloat(copy.startTime, 2), parseFloat(copy.endTime, 2))
+                                        updateRentalBooking(copy)
+                                    }
+                                } />
+                        </div>
+                    </fieldset>
+                </article>
+                <article className='CreateImages'>
+                    <div className='imageContainer'>
+
+                        {backdropsArray.map(backdropObj =>
+                            <img className="pic" src={backdropObj.image} alt="image" />
+                        )}
+                        {propsArray.map(propObj =>
+                            <img className="pic" src={propObj.image} alt="image" />
+                        )}
+                        {furnituresArray.map(furnitureObj =>
+                            <img className="pic" src={furnitureObj.image} alt="image" />
+                        )}
+                        {lightsArray.map(lightObj =>
+                            <img className="pic" src={lightObj.image} alt="image" />
+                        )}
+
+                    </div>
+                </article>
+            </section>
+            <div className='getCentered'>
+                <button 
+                    onClick={(clickEvent) => editButtonClick(clickEvent, rentalPackage, rentalBookingPackage)}
+                    className="buttonz">
+                    Confirm Edits
+                </button>
+            </div>
         </form >
     )
 }
